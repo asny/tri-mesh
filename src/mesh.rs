09b5fa136f8620d388b652996b474bdbd8abf839
Asger <asger.nyman@alexandra.dk>
2019-01-10 15:47:31 +0100

    Transformatons doc + rotate and apply transformation functionality

--- src/mesh.rs
+++ src/mesh.rs
@@ -12,6 +12,10 @@ pub mod math {
 
     /// Vector with three elements.
     pub type Vec3 = Vector3<f32>;
+    pub type Vec4 = Vector4<f32>;
+
+    pub type Mat3 = Matrix3<f32>;
+    pub type Mat4 = Matrix4<f32>;
 }
 
 pub mod ids;
--- src/mesh/transformations.rs
+++ src/mesh/transformations.rs
@@ -7,20 +7,38 @@ use crate::mesh::ids::*;
 /// # Transformations
 impl Mesh
 {
+    /// Moves the vertex to the specified position.
     pub fn move_vertex_to(&mut self, vertex_id: VertexID, value: Vec3)
     {
         self.positions.insert(vertex_id, value);
     }
 
+    /// Moves the vertex by the specified vector, i.e. the new position is `mesh.vertex_position(vertex_id) + value`.
     pub fn move_vertex_by(&mut self, vertex_id: VertexID, value: Vec3)
     {
-        let mut p = value;
-        {
-            p = p + *self.positions.get(&vertex_id).unwrap();
-        }
-        self.positions.insert(vertex_id, p);
+        let p = value + *self.vertex_position(&vertex_id);
+        self.move_vertex_to(vertex_id, p);
     }
 
+    /// Scales the entire mesh by `scale`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use tri_mesh::prelude::*;
+    /// #
+    /// # fn main() -> Result<(), Box<tri_mesh::mesh_builder::Error>> {
+    ///     let mut mesh = MeshBuilder::new().cube().build()?;
+    /// #   let first_face_id = mesh.face_iter().next().unwrap();
+    /// #   let face_area_before_scale = mesh.face_area(&first_face_id);
+    ///     mesh.scale(2.0);
+    /// #   let face_area_after_scale = mesh.face_area(&first_face_id);
+    /// #   assert_eq!(4.0 * face_area_before_scale, face_area_after_scale);
+    /// #   mesh.is_valid().unwrap();
+    /// #   Ok(())
+    /// # }
+    /// ```
+    ///
     pub fn scale(&mut self, scale: f32)
     {
         for vertex_id in self.vertex_iter() {
@@ -29,10 +47,43 @@ impl Mesh
         }
     }
 
-    pub fn translate(&mut self, translation: &Vec3)
+    /// Translates the entire mesh by `translation`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use tri_mesh::prelude::*;
+    /// #
+    /// # fn main() -> Result<(), Box<tri_mesh::mesh_builder::Error>> {
+    ///     let mut mesh = MeshBuilder::new().cube().build()?;
+    /// #   let first_vertex_id = mesh.vertex_iter().next().unwrap();
+    /// #   let vertex_position_before_scale = *mesh.vertex_position(&first_vertex_id);
+    ///     mesh.translate(vec3(2.5, -1.0, 0.0));
+    /// #   let vertex_position_after_scale = *mesh.vertex_position(&first_vertex_id);
+    /// #   assert_eq!(vertex_position_before_scale + vec3(2.5, -1.0, 0.0), vertex_position_after_scale);
+    /// #   mesh.is_valid().unwrap();
+    /// #   Ok(())
+    /// # }
+    /// ```
+    ///
+    pub fn translate(&mut self, translation: Vec3)
     {
         for vertex_id in self.vertex_iter() {
-            self.move_vertex_by(vertex_id, *translation);
+            self.move_vertex_by(vertex_id, translation);
+        }
+    }
+
+    pub fn rotate(&mut self, rotation: Mat3)
+    {
+        self.apply_transformation(rotation.into());
+    }
+
+    pub fn apply_transformation(&mut self, transformation: Mat4)
+    {
+        for vertex_id in self.vertex_iter() {
+            let p = *self.vertex_position(&vertex_id);
+            let p_new = (transformation * p.extend(1.0)).truncate();
+            self.move_vertex_to(vertex_id, p_new);
         }
     }
 }
