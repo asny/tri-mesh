f22a50228cc53927992a99a35048034295c83b4b
Asger <asger.nyman@alexandra.dk>
2019-01-15 09:56:22 +0100

    Doc + test + bugfix in face measures

--- src/mesh/face_measures.rs
+++ src/mesh/face_measures.rs
@@ -7,12 +7,14 @@ use crate::mesh::ids::*;
 /// # Face measures
 impl Mesh
 {
+    /// Returns the positions of the face vertices.
     pub fn face_positions(&self, face_id: FaceID) -> (&Vec3, &Vec3, &Vec3)
     {
         let vertices = self.ordered_face_vertices(face_id);
         (self.vertex_position(vertices.0), self.vertex_position(vertices.1), self.vertex_position(vertices.2))
     }
 
+    /// Returns the normal of the face.
     pub fn face_normal(&self, face_id: FaceID) -> Vec3
     {
         let mut walker = self.walker_from_face(face_id);
@@ -26,6 +28,7 @@ impl Mesh
         dir.normalize()
     }
 
+    /// Returns the area of the face.
     pub fn face_area(&self, face_id: FaceID) -> f32
     {
         let mut walker = self.walker_from_face(face_id);
@@ -35,9 +38,10 @@ impl Mesh
         walker.as_next();
         let v1 = *self.vertex_position(walker.vertex_id().unwrap()) - p0;
 
-        v0.cross(v1).magnitude()
+        0.5 * v0.cross(v1).magnitude()
     }
 
+    /// Returns the center of the face given as the average of its vertex positions.
     pub fn face_center(&self, face_id: FaceID) -> Vec3
     {
         let mut walker = self.walker_from_face(face_id);
@@ -59,11 +63,29 @@ mod tests {
     use crate::MeshBuilder;
 
     #[test]
+    fn test_face_area() {
+        let mesh = MeshBuilder::new().triangle().build().unwrap();
+        let face_id = mesh.face_iter().next().unwrap();
+        assert_eq!(9.0, mesh.face_area(face_id));
+    }
+
+    #[test]
     fn test_face_normal() {
         let mesh = MeshBuilder::new().triangle().build().unwrap();
-        let computed_normal = mesh.face_normal(FaceID::new(0));
+        let face_id = mesh.face_iter().next().unwrap();
+        let computed_normal = mesh.face_normal(face_id);
         assert_eq!(0.0, computed_normal.x);
         assert_eq!(0.0, computed_normal.y);
         assert_eq!(1.0, computed_normal.z);
+    }
+
+    #[test]
+    fn test_face_center() {
+        let mesh = MeshBuilder::new().triangle().build().unwrap();
+        let face_id = mesh.face_iter().next().unwrap();
+        let center = mesh.face_center(face_id);
+        assert_eq!(0.0, center.x);
+        assert_eq!(0.0, center.y);
+        assert_eq!(0.0, center.z);
     }
 }
