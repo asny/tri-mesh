e5bf4575f29bc23277c89ea93569e61f4aed7cbe
Asger <asger.nyman@alexandra.dk>
2019-02-21 15:37:29 +0100

    Refactor in mesh builder documentation

--- src/mesh_builder.rs
+++ src/mesh_builder.rs
@@ -14,99 +14,10 @@ pub enum Error {
     }
 }
 
+///
 /// `MeshBuilder` contains functionality to build a mesh from either raw data (indices, positions, normals)
-/// or from simple geometric shapes (box, icosahedron, cylinder, ..)
+/// or from simple geometric shapes (box, icosahedron, cylinder, ..) or from file source (.obj).
 ///
-/// # Examples
-///
-/// Build from indices and positions:
-/// ```
-/// # use tri_mesh::mesh_builder::{MeshBuilder, Error};
-/// #
-/// # fn main() -> Result<(), Box<Error>> {
-/// let indices: Vec<u32> = vec![0, 1, 2,  0, 2, 3,  0, 3, 1];
-/// let positions: Vec<f32> = vec![0.0, 0.0, 0.0,  1.0, 0.0, -0.5,  -1.0, 0.0, -0.5, 0.0, 0.0, 1.0];
-/// let mesh = MeshBuilder::new().with_indices(indices).with_positions(positions).build()?;
-///
-/// assert_eq!(mesh.no_faces(), 3);
-/// assert_eq!(mesh.no_vertices(), 4);
-///
-/// #   mesh.is_valid().unwrap();
-/// #   Ok(())
-/// # }
-/// ```
-///
-/// Build from positions (note: Use [merge_overlapping_primitives](crate::mesh::Mesh::merge_overlapping_primitives) if you want to merge
-/// unconnected but overlapping parts of the mesh):
-/// ```
-/// # use tri_mesh::mesh_builder::{MeshBuilder, Error};
-/// #
-/// # fn main() -> Result<(), Box<Error>> {
-/// let positions: Vec<f32> = vec![0.0, 0.0, 0.0,  1.0, 0.0, -0.5,  -1.0, 0.0, -0.5,
-///                                    0.0, 0.0, 0.0,  -1.0, 0.0, -0.5, 0.0, 0.0, 1.0,
-///                                    0.0, 0.0, 0.0,  0.0, 0.0, 1.0,  1.0, 0.0, -0.5];
-/// let mesh = MeshBuilder::new().with_positions(positions).build()?;
-///
-/// assert_eq!(mesh.no_faces(), 3);
-/// assert_eq!(mesh.no_vertices(), 9);
-///
-/// #   mesh.is_valid().unwrap();
-/// #   Ok(())
-/// # }
-/// ```
-///
-/// Build a cube:
-/// ```
-/// # use tri_mesh::mesh_builder::{MeshBuilder, Error};
-/// #
-/// # fn main() -> Result<(), Box<Error>> {
-/// let mesh = MeshBuilder::new().cube().build()?;
-///
-/// assert_eq!(mesh.no_faces(), 12);
-/// assert_eq!(mesh.no_vertices(), 8);
-///
-/// #   mesh.is_valid().unwrap();
-/// #   Ok(())
-/// # }
-/// ```
-///
-/// Build from obj source:
-/// ```
-/// # use tri_mesh::mesh_builder::{MeshBuilder, Error};
-/// #
-/// # fn main() -> Result<(), Box<Error>> {
-///
-/// let source = "o Cube
-/// v 1.000000 -1.000000 -1.000000
-/// # v 1.000000 -1.000000 1.000000
-/// # v -1.000000 -1.000000 1.000000
-/// # v -1.000000 -1.000000 -1.000000
-/// # v 1.000000 1.000000 -1.000000
-/// # v 0.999999 1.000000 1.000001
-/// # v -1.000000 1.000000 1.000000
-/// # v -1.000000 1.000000 -1.000000
-/// # f 1 2 3
-/// # f 1 3 4
-/// # f 5 8 7
-/// # f 5 7 6
-/// # f 1 5 6
-/// # f 1 6 2
-/// # f 2 6 7
-/// # f 2 7 3
-/// # f 3 7 8
-/// # f 3 8 4
-/// # f 5 1 4
-/// f 5 4 8".to_string();
-///
-/// let mesh = MeshBuilder::new().with_obj(source).build()?;
-///
-/// assert_eq!(mesh.no_faces(), 12);
-/// assert_eq!(mesh.no_vertices(), 8);
-///
-/// #   mesh.is_valid().unwrap();
-/// #   Ok(())
-/// # }
-/// ```
 #[derive(Debug, Default)]
 pub struct MeshBuilder {
     indices: Option<Vec<u32>>,
@@ -121,22 +32,105 @@ impl MeshBuilder {
         MeshBuilder {indices: None, positions: None}
     }
 
+    ///
     /// Set the indices of each face, where the indices of face `x` is `(i0, i1, i2) = (indices[3*x], indices[3*x+1], indices[3*x+2])`.
+    ///
+    /// # Examples
+    /// ```
+    /// # use tri_mesh::mesh_builder::{MeshBuilder, Error};
+    /// #
+    /// # fn main() -> Result<(), Box<Error>> {
+    /// let indices: Vec<u32> = vec![0, 1, 2,  0, 2, 3,  0, 3, 1];
+    /// let positions: Vec<f32> = vec![0.0, 0.0, 0.0,  1.0, 0.0, -0.5,  -1.0, 0.0, -0.5, 0.0, 0.0, 1.0];
+    /// let mesh = MeshBuilder::new().with_indices(indices).with_positions(positions).build()?;
+    ///
+    /// assert_eq!(mesh.no_faces(), 3);
+    /// assert_eq!(mesh.no_vertices(), 4);
+    ///
+    /// #   mesh.is_valid().unwrap();
+    /// #   Ok(())
+    /// # }
+    /// ```
+    ///
     pub fn with_indices(mut self, indices: Vec<u32>) -> Self
     {
         self.indices = Some(indices);
         self
     }
 
+    ///
     /// Set the positions of each vertex, where the position of vertex `x` is `(x, y, z) = (positions[3*x], positions[3*x+1], positions[3*x+2])`;
+    ///
+    /// # Examples
+    ///
+    /// Build from positions (note: Use [merge_overlapping_primitives](crate::mesh::Mesh::merge_overlapping_primitives) if you want to merge
+    /// unconnected but overlapping parts of the mesh):
+    /// ```
+    /// # use tri_mesh::mesh_builder::{MeshBuilder, Error};
+    /// #
+    /// # fn main() -> Result<(), Box<Error>> {
+    /// let positions: Vec<f32> = vec![0.0, 0.0, 0.0,  1.0, 0.0, -0.5,  -1.0, 0.0, -0.5,
+    ///                                    0.0, 0.0, 0.0,  -1.0, 0.0, -0.5, 0.0, 0.0, 1.0,
+    ///                                    0.0, 0.0, 0.0,  0.0, 0.0, 1.0,  1.0, 0.0, -0.5];
+    /// let mesh = MeshBuilder::new().with_positions(positions).build()?;
+    ///
+    /// assert_eq!(mesh.no_faces(), 3);
+    /// assert_eq!(mesh.no_vertices(), 9);
+    ///
+    /// #   mesh.is_valid().unwrap();
+    /// #   Ok(())
+    /// # }
+    /// ```
+    ///
     pub fn with_positions(mut self, positions: Vec<f32>) -> Self
     {
         self.positions = Some(positions);
         self
     }
 
-    /// Parses the .obj file and extracts the connectivity information (indices) and positions which is used to construct a mesh when the 'build' method is called.
+    ///
+    /// Parses the .obj file and extracts the connectivity information (indices) and positions which is used to construct a mesh.
     /// If the .obj file contains multiple objects, all objects are added to the mesh, but they will not be connected.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use tri_mesh::mesh_builder::{MeshBuilder, Error};
+    /// #
+    /// # fn main() -> Result<(), Box<Error>> {
+    /// let source = "o Cube
+    /// v 1.000000 -1.000000 -1.000000
+    /// #...
+    /// # v 1.000000 -1.000000 1.000000
+    /// # v -1.000000 -1.000000 1.000000
+    /// # v -1.000000 -1.000000 -1.000000
+    /// # v 1.000000 1.000000 -1.000000
+    /// # v 0.999999 1.000000 1.000001
+    /// # v -1.000000 1.000000 1.000000
+    /// # v -1.000000 1.000000 -1.000000
+    /// # f 1 2 3
+    /// # f 1 3 4
+    /// # f 5 8 7
+    /// # f 5 7 6
+    /// # f 1 5 6
+    /// # f 1 6 2
+    /// # f 2 6 7
+    /// # f 2 7 3
+    /// # f 3 7 8
+    /// # f 3 8 4
+    /// # f 5 1 4
+    /// f 5 4 8".to_string();
+    ///
+    /// let mesh = MeshBuilder::new().with_obj(source).build()?;
+    ///
+    /// assert_eq!(mesh.no_faces(), 12);
+    /// assert_eq!(mesh.no_vertices(), 8);
+    ///
+    /// #   mesh.is_valid().unwrap();
+    /// #   Ok(())
+    /// # }
+    /// ```
+    ///
     pub fn with_obj(mut self, source: String) -> Self
     {
         let mut positions = Vec::new();
@@ -183,7 +177,25 @@ impl MeshBuilder {
         Ok(Mesh::new(indices, positions))
     }
 
+    ///
     /// Creates a cube.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use tri_mesh::mesh_builder::{MeshBuilder, Error};
+    /// #
+    /// # fn main() -> Result<(), Box<Error>> {
+    /// let mesh = MeshBuilder::new().cube().build()?;
+    ///
+    /// assert_eq!(mesh.no_faces(), 12);
+    /// assert_eq!(mesh.no_vertices(), 8);
+    ///
+    /// #   mesh.is_valid().unwrap();
+    /// #   Ok(())
+    /// # }
+    /// ```
+    ///
     pub fn cube(self) -> Self
     {
         self.with_positions(vec![
