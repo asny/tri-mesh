91550b94263f6b7722a07ca7942d4fb955048924
Asger Nyman Christiansen <asgernyman@gmail.com>
2022-01-25 17:33:23 +0100

    obj functionality plain functions

--- src/io/obj.rs
+++ src/io/obj.rs
@@ -1,72 +1,70 @@
 use crate::mesh::Mesh;
 use crate::TriMeshResult;
 
-impl Mesh {
-    ///
-    /// Parses the .obj file and extracts the connectivity information (indices) and positions which is used to construct a mesh when the `build` method is called.
-    /// If the .obj file contains multiple objects, all objects are added to the mesh, but they will not be connected.
-    ///
-    /// # Examples
-    ///
-    /// ```no_run
-    /// # use tri_mesh::*;
-    /// # fn main() -> tri_mesh::TriMeshResult<()> {
-    ///     let obj_source = std::fs::read_to_string("foo.obj").expect("Something went wrong reading the file");
-    ///     let mesh = Mesh::from_obj(obj_source)?;
-    /// #    Ok(())
-    /// # }
-    /// ```
-    pub fn from_obj(source: String) -> TriMeshResult<Self> {
-        Self::from_named_obj(source, "")
-    }
+///
+/// Parses the .obj file and returns a [Mesh].
+/// If the .obj file contains multiple objects, all objects are added to the mesh, but they will not be connected.
+///
+/// # Examples
+///
+/// ```no_run
+/// # use tri_mesh::*;
+/// # fn main() -> tri_mesh::TriMeshResult<()> {
+///     let obj_source = std::fs::read_to_string("foo.obj").expect("Something went wrong reading the file");
+///     let mesh = from_obj(obj_source)?;
+/// #    Ok(())
+/// # }
+/// ```
+pub fn from_obj(source: String) -> TriMeshResult<Mesh> {
+    from_named_obj(source, "")
+}
 
-    ///
-    /// Parses the .obj file and extracts the connectivity information (indices) and positions which is used to construct a mesh when the `build` method is called.
-    /// Only the object with the given name is extracted from the file.
-    ///
-    /// # Examples
-    ///
-    /// ```no_run
-    /// # use tri_mesh::*;
-    /// # fn main() -> tri_mesh::TriMeshResult<()> {
-    ///     let obj_source = std::fs::read_to_string("foo.obj").expect("Something went wrong reading the file");
-    ///     let mesh = Mesh::from_named_obj(obj_source, "my_object")?;
-    /// #    Ok(())
-    /// # }
-    /// ```
-    pub fn from_named_obj(source: String, object_name: &str) -> TriMeshResult<Self> {
-        let objs = wavefront_obj::obj::parse(source).unwrap();
-        let mut positions = Vec::new();
-        let mut indices = Vec::new();
+///
+/// Parses the .obj file and returns a [Mesh].
+/// Only the object with the given name is extracted from the file.
+///
+/// # Examples
+///
+/// ```no_run
+/// # use tri_mesh::*;
+/// # fn main() -> tri_mesh::TriMeshResult<()> {
+///     let obj_source = std::fs::read_to_string("foo.obj").expect("Something went wrong reading the file");
+///     let mesh = from_named_obj(obj_source, "my_object")?;
+/// #    Ok(())
+/// # }
+/// ```
+pub fn from_named_obj(source: String, object_name: &str) -> TriMeshResult<Mesh> {
+    let objs = wavefront_obj::obj::parse(source).unwrap();
+    let mut positions = Vec::new();
+    let mut indices = Vec::new();
 
-        for obj in objs.objects.iter() {
-            // Objects consisting of several meshes with different materials
-            if &obj.name == object_name || object_name == "" {
-                let start_index = positions.len() / 3;
-                obj.vertices.iter().for_each(|v| {
-                    positions.push(v.x);
-                    positions.push(v.y);
-                    positions.push(v.z);
-                });
+    for obj in objs.objects.iter() {
+        // Objects consisting of several meshes with different materials
+        if &obj.name == object_name || object_name == "" {
+            let start_index = positions.len() / 3;
+            obj.vertices.iter().for_each(|v| {
+                positions.push(v.x);
+                positions.push(v.y);
+                positions.push(v.z);
+            });
 
-                for mesh in obj.geometry.iter() {
-                    // All meshes with different materials
-                    for primitive in mesh.shapes.iter() {
-                        // All triangles with same material
-                        match primitive.primitive {
-                            wavefront_obj::obj::Primitive::Triangle(i0, i1, i2) => {
-                                indices.push((start_index + i0.0) as u32);
-                                indices.push((start_index + i1.0) as u32);
-                                indices.push((start_index + i2.0) as u32);
-                            }
-                            _ => {}
+            for mesh in obj.geometry.iter() {
+                // All meshes with different materials
+                for primitive in mesh.shapes.iter() {
+                    // All triangles with same material
+                    match primitive.primitive {
+                        wavefront_obj::obj::Primitive::Triangle(i0, i1, i2) => {
+                            indices.push((start_index + i0.0) as u32);
+                            indices.push((start_index + i1.0) as u32);
+                            indices.push((start_index + i2.0) as u32);
                         }
+                        _ => {}
                     }
                 }
             }
         }
-        Ok(Mesh::new(indices, positions))
     }
+    Ok(Mesh::new(indices, positions))
 }
 
 #[cfg(test)]
@@ -98,7 +96,7 @@ mod tests {
         f 5 4 8"
             .to_string();
 
-        let mesh = Mesh::from_obj(source).unwrap();
+        let mesh = from_obj(source).unwrap();
 
         assert_eq!(mesh.no_faces(), 12);
         assert_eq!(mesh.no_vertices(), 8);
