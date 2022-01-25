644acd39fcda0b7fc02f9c4a4d112c1fdc99eb32
Asger Nyman Christiansen <asgernyman@gmail.com>
2022-01-25 17:44:48 +0100

    Doc

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,6 +1,4 @@
-//! Contains the most common functionality used in `tri-mesh`. By
-//! global importing this module, you can avoid the need to import each trait
-//! individually, while still being selective about what types you import.
+//! Contains the most common `tri-mesh` functionality.
 
 pub use crate::math::*;
 pub use crate::mesh::*;
