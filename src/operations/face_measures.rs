7def5fdb5d372a30e4be526824edf3982a6ad4a2
Asger Nyman Christiansen <asgernyman@gmail.com>
2022-01-25 21:09:42 +0100

    Revert FaceMeasures trait for now

--- src/operations/face_measures.rs
+++ src/operations/face_measures.rs
@@ -2,23 +2,10 @@
 
 use crate::mesh::*;
 
-/// Common things that can be computed on a face
-pub trait FaceMeasures {
-    /// Returns the positions of the face vertices.
-    fn face_positions(&self, face_id: FaceID) -> (Vec3, Vec3, Vec3);
-    /// Returns the unnormalized normal of the face.
-    fn face_direction(&self, face_id: FaceID) -> Vec3;
-    /// Returns the normal of the face.
-    fn face_normal(&self, face_id: FaceID) -> Vec3;
-    /// Returns the area of the face.
-    fn face_area(&self, face_id: FaceID) -> f64;
-    /// Returns the center of the face given as the average of its vertex positions.
-    fn face_center(&self, face_id: FaceID) -> Vec3;
-}
-
 /// # Face measures
-impl FaceMeasures for Mesh {
-    fn face_positions(&self, face_id: FaceID) -> (Vec3, Vec3, Vec3) {
+impl Mesh {
+    /// Returns the positions of the face vertices.
+    pub fn face_positions(&self, face_id: FaceID) -> (Vec3, Vec3, Vec3) {
         let vertices = self.ordered_face_vertices(face_id);
         (
             self.vertex_position(vertices.0),
@@ -27,7 +14,8 @@ impl FaceMeasures for Mesh {
         )
     }
 
-    fn face_direction(&self, face_id: FaceID) -> Vec3 {
+    /// Returns the unnormalized normal of the face.
+    pub fn face_direction(&self, face_id: FaceID) -> Vec3 {
         let mut walker = self.walker_from_face(face_id);
         let p0 = self.vertex_position(walker.vertex_id().unwrap());
         walker.as_next();
@@ -37,15 +25,19 @@ impl FaceMeasures for Mesh {
 
         v0.cross(v1)
     }
-    fn face_normal(&self, face_id: FaceID) -> Vec3 {
+
+    /// Returns the normal of the face.
+    pub fn face_normal(&self, face_id: FaceID) -> Vec3 {
         self.face_direction(face_id).normalize()
     }
 
-    fn face_area(&self, face_id: FaceID) -> f64 {
+    /// Returns the area of the face.
+    pub fn face_area(&self, face_id: FaceID) -> f64 {
         0.5 * self.face_direction(face_id).magnitude()
     }
 
-    fn face_center(&self, face_id: FaceID) -> Vec3 {
+    /// Returns the center of the face given as the average of its vertex positions.
+    pub fn face_center(&self, face_id: FaceID) -> Vec3 {
         let mut walker = self.walker_from_face(face_id);
         let p0 = self.vertex_position(walker.vertex_id().unwrap());
         walker.as_next();
--- src/prelude.rs
+++ src/prelude.rs
@@ -3,4 +3,3 @@
 pub use crate::math::*;
 pub use crate::mesh::*;
 pub use crate::mesh_builder::*;
-pub use crate::operations::FaceMeasures;
