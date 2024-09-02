8faabd3a92e5b7e9947f3c00273076c67d18f3a1
Asger Nyman Christiansen <asgernyman@gmail.com>
2024-09-02 11:56:48 +0200

    Fix warnings

--- src/operations.rs
+++ src/operations.rs
@@ -4,50 +4,30 @@
 
 // Simple
 mod connectivity;
-#[doc(inline)]
-pub use connectivity::*;
 
 mod vertex_measures;
-#[doc(inline)]
-pub use vertex_measures::*;
 
 mod edge_measures;
-#[doc(inline)]
-pub use edge_measures::*;
 
 mod face_measures;
-#[doc(inline)]
-pub use face_measures::*;
 
 mod transformations;
-#[doc(inline)]
-pub use transformations::*;
 
 mod bounding_box;
 #[doc(inline)]
 pub use bounding_box::*;
 
 mod validity;
-#[doc(inline)]
-pub use validity::*;
 
 // Advanced
 mod quality;
-#[doc(inline)]
-pub use quality::*;
 
 mod connected_components;
-#[doc(inline)]
-pub use connected_components::*;
 
 mod intersection;
 #[doc(inline)]
 pub use intersection::*;
 
 mod merge;
-#[doc(inline)]
-pub use merge::*;
 
 mod split;
-#[doc(inline)]
-pub use split::*;
